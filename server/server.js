30f8ea4d606dfb912149b97e1e780b7207a2ed89
NickRussert <russertnick@gmail.com>
2025-03-18 21:19:24 -0700

    back to working

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
 const WebSocket = require('ws');
 
-const server = new WebSocket.Server({ host: '0.0.0.0', port: 8080 });
+const server = new WebSocket.Server({ port: 8080 });
 
 server.on('connection', (socket) => {
     console.log('A player connected');
 
     socket.on('message', (message) => {
-    let data = JSON.parse(message.toString()); // Convert Buffer to String & Parse JSON
-    console.log('Received:', data);
+        let data = JSON.parse(message.toString()); // Convert Buffer to String & Parse JSON
+        console.log('Received:', data);
 
-    // Example: Log the vote choice
-    if (data.type === "vote") {
-        console.log(`Vote received for: ${data.choice}`);
-    }
-});
-
+        if (data.type === "vote") {
+            console.log(`Vote received for: ${data.choice}`);
+        }
+    });
 
     socket.on('close', () => {
         console.log('A player disconnected');
