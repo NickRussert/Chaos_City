ba74734491f30b56d0657bb660994ed5370e5b3d
NickRussert <russertnick@gmail.com>
2025-03-18 20:43:31 -0700

    updated server vote log

--- server/server.js
+++ server/server.js
@@ -6,8 +6,15 @@ server.on('connection', (socket) => {
     console.log('A player connected');
 
     socket.on('message', (message) => {
-        console.log('Received:', message);
-    });
+    let data = JSON.parse(message.toString()); // Convert Buffer to String & Parse JSON
+    console.log('Received:', data);
+
+    // Example: Log the vote choice
+    if (data.type === "vote") {
+        console.log(`Vote received for: ${data.choice}`);
+    }
+});
+
 
     socket.on('close', () => {
         console.log('A player disconnected');
